72def2b44bfdb26d2b3cd05220a98145bd467b28
faizdamar1 <faizdamar123@gmail.com>
2023-08-14 21:30:10 +0700

    home screen

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 1);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Whatsapp clone"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              if (kDebugMode) {
+                print(value);
+              }
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem(
+                  value: "New group",
+                  child: Text("New group"),
+                ),
+                const PopupMenuItem(
+                  value: "New broadcast",
+                  child: Text("New broadcast"),
+                ),
+                const PopupMenuItem(
+                  value: "Whatsapp Web",
+                  child: Text("Whatsapp Web"),
+                ),
+                const PopupMenuItem(
+                  value: "Starred messages",
+                  child: Text("Starred messages"),
+                ),
+                const PopupMenuItem(
+                  value: "Settings",
+                  child: Text("Settings"),
+                ),
+              ];
+            },
+          )
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: "CHATS",
+            ),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          Text("Camera"),
+          Text("chat"),
+          Text("Status"),
+          Text("Calls"),
+        ],
+      ),
+    );
+  }
+}
